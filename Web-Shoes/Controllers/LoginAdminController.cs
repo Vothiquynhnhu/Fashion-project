dd2da0def6f4c934e40d57104e6d574cbb3ccbe2
Vothiquynhnhu <amvanimemusicsad@gmail.com>
2021-11-01 09:10:52 +0700

    Done front end login admin

---
+++ Web-Shoes/Controllers/LoginAdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Shoes.Controllers
+{
+    public class LoginAdminController : Controller
+    {
+
+        [Route("/loginadmin")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web-Shoes/Controllers/SubscribersManagementController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Shoes.Data;
+using Web_Shoes.Entity;
+using Web_Shoes.Models;
+
+namespace Web_Shoes.Controllers
+{
+    public class SubscribersManagementController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public SubscribersManagementController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/subsribersmanagement")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            try
+            {
+                var subsriber = from a in _context.Subscribers select a;
+
+                return View(subsriber);
+            }
+            catch 
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
